cc992939dd7e0e98ffd91524767be26213efd122
ArisNunez <arisnunez@Ariss-iMac.local>
2014-07-23 17:14:41 -0400

    Footer Link Update

--- deconstruct/footer.php
+++ deconstruct/footer.php
@@ -2,13 +2,13 @@
 			<div class="center_contain">
 				<div class="footer_left">
 					<ul class="footer_links">
-						<li><a href="index.php?page=15">Contact Us</a></li>
-						<li><a href="index.php?page=17">Return Policy</a></li>
-						<li><a href="index.php?page=19">Customer Service</a></li>
+						<li><a href="index.php?page=31">Contact Us</a></li>
+						<li><a href="index.php?page=33">Return Policy</a></li>
+						<li><a href="index.php?page=37">Customer Service</a></li>
 					</ul>
 					<ul class="footer_links">
-						<li><a href="index.php?page=21">Privacy Policy</a></li>
-						<li><a href="index.php?page=23">Terms &amp; Conditions</a></li>
+						<li><a href="index.php?page=35">Privacy Policy</a></li>
+						<li><a href="index.php?page=39">Terms &amp; Conditions</a></li>
 					</ul>
 				</div>
 				<div class="footer_right">
